9beb803bead18ef499884180c72cacf359bd3984
dipuAppDev <dipuappdev@gmail.com>
2023-11-25 19:08:55 +0600

    file update

---
+++ Assignment2.kt
@@ -0,0 +1,17 @@
+import java.util.Scanner
+fun main(args:Array<String>){
+
+    var sc = Scanner(System.`in`)
+    print("Enter starting year : ")
+    var startingYear = sc.nextInt()
+    print("Enter Ending Year : ")
+    var endingYear = sc.nextInt()
+    println("Leap year from $startingYear to $endingYear is : \n")
+    for(year in startingYear..endingYear step 2){
+        if ((year%4==0)&&(year%100!=0)||(year%400==0)){
+            println("$year is a leap year.")
+        }
+        else{
+            println("$year is not a leap year.")
+        }
+    }
